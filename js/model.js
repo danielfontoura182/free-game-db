88d0b748970c8e99b3cb76679696a527df8c6653
Daniel Fontoura <fontouradaniel@hotmail.com>
2021-08-19 19:23:41 -0300

    add controller.js, model.js, secret.js and .gitignore files

--- js/model.js
+++ js/model.js
@@ -1,3 +1,5 @@
+import API_KEY from './secrets.js'
+
 async function getData() {
   const res = await fetch(
     'https://free-to-play-games-database.p.rapidapi.com/api/games',
@@ -5,7 +7,7 @@ async function getData() {
       method: 'GET',
       headers: {
         'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
-        'x-rapidapi-key': 'a123bf2cb2mshd5fa2af593ef59ap120794jsn6fe472401051',
+        'x-rapidapi-key': API_KEY,
       },
     }
   )
