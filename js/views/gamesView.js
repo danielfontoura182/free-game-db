f6a012ae6866709b36f309bd272d3ce8c96ef15c
Daniel Fontoura <fontouradaniel@hotmail.com>
2021-09-04 10:01:59 -0300

    fix game name not showing

--- js/views/gamesView.js
+++ js/views/gamesView.js
@@ -7,23 +7,25 @@ class gamesView extends View {
     const markup = this._data
       .map(
         (game) => ` 
-      <div class="game" data-id="${game.id}">
-      <img
-        src="${game.thumbnail}"
-        alt=""
-        class="game__image"
-      />
-      <div class="game__name">${game.title}</div>
-      <div class="game-info-hover" data-id="${game.id}">
-      ${
-        game.short_description.length < 120
-          ? game.short_description
-          : game.short_description.slice(0, 120).trim() + '...'
-      } <br><br>
-      <span>Click to see more.</span>
-    </div>
-    </div>
-    
+      <div class="game-container">  
+        <div class="game" data-id="${game.id}">
+        <img
+          src="${game.thumbnail}"
+          alt=""
+          class="game__image"
+        />
+        
+        <div class="game-info-hover" data-id="${game.id}">
+        ${
+          game.short_description.length < 120
+            ? game.short_description
+            : game.short_description.slice(0, 120).trim() + '...'
+        } <br><br>
+        <span>Click to see more.</span>
+      </div>
+      </div>
+      <div class="game-container__name">${game.title}</div>
+      </div>
       `
       )
       .join('')
